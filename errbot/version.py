67fae78005b8ac8e00028b83d6bf411eb9dcaa02
Matthias Kay <matthias.kay@hlag.com>
2023-10-16 23:54:01 +0200

    KAYMAN-MK-PATCH-1

--- errbot/version.py
+++ errbot/version.py
@@ -2,4 +2,4 @@ import os
 
 # Just the current version of Errbot.
 # It is used for deployment on pypi AND for version checking at plugin load time.
-VERSION = os.getEnv('RELEASE_VERSION')  # leave it at 9.9.9 on master until it is branched to a version.
+VERSION = os.getenv('RELEASE_VERSION')  # leave it at 9.9.9 on master until it is branched to a version.
